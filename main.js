84c1128b7c05d7cd69225023ebf2361ab9811592
chininchu <120990876+chininchu@users.noreply.github.com>
2023-08-11 19:55:58 +0500

    Created a function called checkSize

---
+++ main.js
@@ -0,0 +1,17 @@
+"use strict";
+
+
+// Create a function called checkSize() with the parameter fileSize.
+
+
+function checkSize(fileSize) {
+}
+
+// Create a constant called maxSize that holds the maximum allowed gigabytes and set it to 5.
+
+function checkSize(fileSize) {
+
+    const maxSize = 5;
+
+
+}
